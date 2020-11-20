2e35427e9648b18ee6d329fc623c730e196537c6
aditya-aot <aditya.aot.11@gmail.com>
2020-11-20 21:32:09 +0530

    part 2

--- covid_19_site/settings.py
+++ covid_19_site/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -51,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'covid_19_site.urls'
@@ -126,5 +130,9 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
+STATICFILES_STORAGE = 'whitenoise.storange.CompressedManifestStorage'
+
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+django_heroku.settings(locals())
