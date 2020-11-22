e56a7d04f50d54ea628249bed81b11e42c974de5
aditya-aot <aditya.aot.11@gmail.com>
2020-11-22 20:46:19 +0530

    part 3

--- static/styles/main.js
+++ static/styles/main.js
@@ -1,9 +1,9 @@
-document.onreadystatechange = function() { 
-    if (document.readyState !== "complete") { 
-        document.querySelector("script").style.visibility = "hidden"; 
-        document.querySelector("spinner").style.visibility = "visible"; 
-    } else { 
-        document.querySelector("spinner").style.display = "none"; 
-        document.querySelector("script").style.visibility = "visible"; 
-    } 
-}; 
+// document.onreadystatechange = function() { 
+//     if (document.readyState !== "complete") { 
+//         document.querySelector("script").style.visibility = "hidden"; 
+//         document.querySelector("spinner").style.visibility = "visible"; 
+//     } else { 
+//         document.querySelector("spinner").style.display = "none"; 
+//         document.querySelector("script").style.visibility = "visible"; 
+//     } 
+// }; 
